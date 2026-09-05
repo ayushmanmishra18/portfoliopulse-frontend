a0d925e60c115418f1429826257b1c541db60d65
AYUSHAMAN MISHRA <149884884+ayushmanmishra18@users.noreply.github.com>
2026-09-05 21:54:16 +0000

    fixed 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,26 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    emptyOutDir: false,
-  },
+export default defineConfig(({ mode }) => {
+  const apiUrl = loadEnv(mode, process.cwd(), '').VITE_API_URL
+
+  return {
+    plugins: [
+      react(),
+      {
+        name: 'replace-api-url-in-legacy-bundle',
+        transform(code, id) {
+          if (!id.endsWith('/dist/assets/index-CESIacOY.js')) {
+            return
+          }
+
+          return code.replaceAll('http://localhost:5000/api', apiUrl)
+        },
+      },
+    ],
+    build: {
+      emptyOutDir: false,
+    },
+  }
 })
